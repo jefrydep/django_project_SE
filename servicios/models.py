f61580e9f769f4864876f145c2a49f23715a82d8
jefry <jefrydep@gmail.com>
2023-03-04 18:09:54 -0500

    adding models

---
+++ servicios/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Servicio (models.Model):
+    Titulo=models.CharField(max_length=50)
+    Contenido=models.CharField(max_length=50)
+    Imagen=models.ImageField()
+    Created= models.DateTimeField(auto_now_add= True)
+    Updated=models.DateTimeField(auto_now_add=True)
+
+class Meta:
+    verbose_name='servicio'
+    verbose_name_plural='servicios'
+def __str__(self):
+    return self.Titulos
